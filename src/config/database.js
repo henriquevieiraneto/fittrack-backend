3b98779a786dede605d6c540fd2484c1892cd9e2
HENRIQUE VIEIRA NETO <henrique_vieira-n@estudante.sesisenai.org.br>
2025-11-07 22:18:44 -0300

    a

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@
 const config = {
     host: 'localhost',      // Seu endereço do MySQL
     user: 'root',           // Seu usuário do MySQL
-    password: 'sua_senha_secreta', // Sua senha do MySQL
+    password: 'senai', // Sua senha do MySQL
     database: 'fittrack_db',// Nome do seu banco de dados
     port: 3306
 };
